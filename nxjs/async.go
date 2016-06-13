e693d704c6d4507ae403bdbeb7a6667035f2af58
jaracil <pepe@nayarsystems.com>
2016-06-13 16:54:16 +0200

    nxjs refactor.

--- nxjs/async.go
+++ nxjs/async.go
@@ -1,269 +1,217 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"time"
 
 	"github.com/jaracil/nxcli/nxcore"
 
 	"github.com/gopherjs/gopherjs/js"
+	"github.com/jaracil/ei"
 )
 
 func ret(r interface{}, e error, cb []*js.Object) {
 	switch len(cb) {
 	case 1:
-		cb[0].Invoke(r, e)
+		cb[0].Invoke(r, WrapError(e))
 
 	case 2:
 		if e == nil {
 			cb[0].Invoke(r)
 		} else {
-			cb[1].Invoke(e)
+			cb[1].Invoke(WrapError(e))
 		}
 	}
 }
 
-func ret3(r1 interface{}, r2 interface{}, e error, cb []*js.Object) {
-	switch len(cb) {
-	case 1:
-		cb[0].Invoke(r1, r2, e)
-
-	case 2:
-		if e == nil {
-			cb[0].Invoke(r1, r2)
-		} else {
-			cb[1].Invoke(e)
-		}
+func WrapError(e error) *js.Object {
+	if e == nil {
+		return nil
 	}
+	jserr := js.Global.Get("Object").New()
+	if err, ok := e.(*nxcore.JsonRpcErr); ok {
+		jserr.Set("code", err.Cod)
+		jserr.Set("message", err.Mess)
+		jserr.Set("data", err.Dat)
+	} else {
+		jserr.Set("code", 0)
+		jserr.Set("message", e.Error())
+		jserr.Set("data", nil)
+	}
+	return jserr
 }
 
-func PatchTaskAsync(jstask *js.Object, task *nxcore.Task) {
-
-	jstask.Set("SendResult", func(res interface{}, cb ...*js.Object) {
+func WrapTask(task *nxcore.Task) *js.Object {
+	if task == nil {
+		return nil
+	}
+	jstask := js.Global.Get("Object").New()
+	jstask.Set("sendResult", func(res interface{}, cb ...*js.Object) {
 		go func() {
 			r, e := task.SendResult(res)
 			ret(r, e, cb)
 		}()
 	})
-
-	jstask.Set("SendError", func(code int, msg string, data interface{}, cb ...*js.Object) {
+	jstask.Set("sendError", func(code int, msg string, data interface{}, cb ...*js.Object) {
 		go func() {
 			r, e := task.SendError(code, msg, data)
 			ret(r, e, cb)
 		}()
 	})
-
-	jstask.Set("Path", task.Path)
-	jstask.Set("Method", task.Method)
-	jstask.Set("Params", task.Params)
-	jstask.Set("Tags", task.Tags)
-
+	jstask.Set("path", task.Path)
+	jstask.Set("method", task.Method)
+	jstask.Set("params", task.Params)
+	jstask.Set("tags", task.Tags)
+	return jstask
 }
 
-func PatchPipeAsync(jspipe *js.Object, pipe *nxcore.Pipe) {
-	jspipe.Set("Close", func(cb ...*js.Object) {
+func WrapPipe(pipe *nxcore.Pipe) *js.Object {
+	if pipe == nil {
+		return nil
+	}
+	jspipe := js.Global.Get("Object").New()
+	jspipe.Set("close", func(cb ...*js.Object) {
 		go func() {
 			r, e := pipe.Close()
 			ret(r, e, cb)
 		}()
 	})
-
-	jspipe.Set("Read", func(max int, timeout int, cb ...*js.Object) {
+	jspipe.Set("read", func(max int, timeout float64, cb ...*js.Object) {
 		go func() {
-			r, e := pipe.Read(max, time.Duration(timeout)*time.Second)
+			r, e := pipe.Read(max, time.Duration(timeout*float64(time.Second)))
 			if e != nil {
 				ret(nil, e, cb)
 				return
 			}
-
-			jd, _ := json.Marshal(r)
-			pd := make(map[string]interface{})
-			json.Unmarshal(jd, &pd)
-
-			ret(pd, e, cb)
+			msgs := make([]ei.M, 0)
+			for _, msg := range r.Msgs {
+				msgs = append(msgs, ei.M{"count": msg.Count, "msg": msg.Msg})
+			}
+			result := ei.M{"msgs": msgs, "waiting": r.Waiting, "drops": r.Drops}
+			ret(result, e, cb)
 		}()
 	})
-
-	jspipe.Set("Write", func(msg interface{}, cb ...*js.Object) {
+	jspipe.Set("write", func(msg interface{}, cb ...*js.Object) {
 		go func() {
 			r, e := pipe.Write(msg)
 			ret(r, e, cb)
 		}()
 	})
-
-	jspipe.Set("Id", func(cb ...*js.Object) string {
-		go func() {
-			r := pipe.Id()
-			fmt.Println("ID", r)
-			ret(r, nil, cb)
-		}()
-
-		return pipe.Id()
-	})
+	jspipe.Set("id", pipe.Id())
+	return jspipe
 }
 
-func PatchNexusAsync(jsnc *js.Object, nc *nxcore.NexusConn) {
-
-	jsnc.Set("Login", func(user string, pass string, cb ...*js.Object) {
+func WrapNexusConn(nc *nxcore.NexusConn) *js.Object {
+	if nc == nil {
+		return nil
+	}
+	jsnc := js.Global.Get("Object").New()
+	jsnc.Set("login", func(user string, pass string, cb ...*js.Object) {
 		go func() {
 			r, e := nc.Login(user, pass)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("TaskPush", func(method string, params interface{}, timeout int, cb ...*js.Object) {
+	jsnc.Set("taskPush", func(method string, params interface{}, timeout float64, cb ...*js.Object) {
 		go func() {
-			r, e := nc.TaskPush(method, params, time.Duration(timeout)*time.Second)
+			r, e := nc.TaskPush(method, params, time.Duration(timeout*float64(time.Second)))
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("TaskPull", func(prefix string, timeout int, cb ...*js.Object) {
+	jsnc.Set("taskPull", func(prefix string, timeout float64, cb ...*js.Object) {
 		go func() {
-			r, e := nc.TaskPull(prefix, time.Duration(timeout)*time.Second)
-			if e != nil {
-				ret(nil, e, cb)
-				return
-			}
-
-			jstask := js.MakeWrapper(r)
-			PatchTaskAsync(jstask, r)
-
-			ret(jstask, e, cb)
+			r, e := nc.TaskPull(prefix, time.Duration(timeout*float64(time.Second)))
+			ret(WrapTask(r), e, cb)
 		}()
 	})
-
-	jsnc.Set("UserCreate", func(user string, pass string, cb ...*js.Object) {
+	jsnc.Set("userCreate", func(user string, pass string, cb ...*js.Object) {
 		go func() {
 			r, e := nc.UserCreate(user, pass)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("UserDelete", func(user string, cb ...*js.Object) {
+	jsnc.Set("userDelete", func(user string, cb ...*js.Object) {
 		go func() {
 			r, e := nc.UserDelete(user)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("UserDelTags", func(user string, prefix string, tags []string, cb ...*js.Object) {
+	jsnc.Set("userDelTags", func(user string, prefix string, tags []string, cb ...*js.Object) {
 		go func() {
 			r, e := nc.UserDelTags(user, prefix, tags)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("UserSetPass", func(user string, pass string, cb ...*js.Object) {
+	jsnc.Set("userSetPass", func(user string, pass string, cb ...*js.Object) {
 		go func() {
 			r, e := nc.UserSetPass(user, pass)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("UserSetTags", func(user string, prefix string, tags map[string]interface{}, cb ...*js.Object) {
+	jsnc.Set("userSetTags", func(user string, prefix string, tags map[string]interface{}, cb ...*js.Object) {
 		go func() {
 			r, e := nc.UserSetTags(user, prefix, tags)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("PipeCreate", func(jopts interface{}, cb ...*js.Object) {
+	jsnc.Set("pipeCreate", func(jopts ei.M, cb ...*js.Object) {
 		go func() {
-			d, e := json.Marshal(jopts)
-			if e != nil {
-				ret(nil, e, cb)
-				return
+			opts := &nxcore.PipeOpts{
+				Length: ei.N(jopts).M("length").IntZ(),
 			}
-			var opts *nxcore.PipeOpts
-			json.Unmarshal(d, &opts)
-
 			r, e := nc.PipeCreate(opts)
-			jspipe := js.MakeWrapper(r)
-
-			PatchPipeAsync(jspipe, r)
-
-			ret(jspipe, e, cb)
+			ret(WrapPipe(r), e, cb)
 		}()
 	})
-
-	jsnc.Set("PipeOpen", func(id string, cb ...*js.Object) {
+	jsnc.Set("pipeOpen", func(id string, cb ...*js.Object) {
 		go func() {
 			r, e := nc.PipeOpen(id)
-			if e != nil {
-				ret(nil, e, cb)
-				return
-			}
-
-			jspipe := js.MakeWrapper(r)
-			PatchPipeAsync(jspipe, r)
-			ret(jspipe, e, cb)
+			ret(WrapPipe(r), e, cb)
 		}()
 	})
-
-	jsnc.Set("ChanPublish", func(channel string, msg interface{}, cb ...*js.Object) {
+	jsnc.Set("chanPublish", func(channel string, msg interface{}, cb ...*js.Object) {
 		go func() {
 			r, e := nc.ChanPublish(channel, msg)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("ChanSubscribe", func(pipe *nxcore.Pipe, channel string, cb ...*js.Object) {
+	jsnc.Set("chanSubscribe", func(jspipe *js.Object, channel string, cb ...*js.Object) { // !!!Warning, Don't work!!!
 		go func() {
-			r, e := nc.ChanSubscribe(pipe, channel)
+			par := ei.M{
+				"pipeid": jspipe.Get("id").String(),
+				"chan":   channel,
+			}
+			r, e := nc.Exec("chan.sub", par)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("ChanUnsubscribe", func(pipe *nxcore.Pipe, channel string, cb ...*js.Object) {
+	jsnc.Set("chanUnsubscribe", func(jspipe *js.Object, channel string, cb ...*js.Object) {
 		go func() {
-			r, e := nc.ChanUnsubscribe(pipe, channel)
+			par := ei.M{
+				"pipeid": jspipe.Get("id").String(),
+				"chan":   channel,
+			}
+			r, e := nc.Exec("chan.unsub", par)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("Exec", func(method string, params interface{}, cb ...*js.Object) {
+	jsnc.Set("exec", func(method string, params interface{}, cb ...*js.Object) {
 		go func() {
 			r, e := nc.Exec(method, params)
 			ret(r, e, cb)
 		}()
 	})
-
-	jsnc.Set("ExecNoWait", func(method string, params interface{}, cb ...*js.Object) {
+	jsnc.Set("close", func(cb ...*js.Object) {
 		go func() {
-			r1, r2, e := nc.ExecNoWait(method, params)
-
-			d, e := json.Marshal(r2)
-			if e != nil {
-				ret(nil, e, cb)
-				return
-			}
-			var res *nxcore.JsonRpcRes
-			json.Unmarshal(d, &res)
-
-			ret3(r1, res, e, cb)
-		}()
-	})
-
-	jsnc.Set("Cancel", func(cb ...*js.Object) {
-		go func() {
-			nc.Cancel()
+			nc.Close()
 			ret(nil, nil, cb)
 		}()
 	})
-
-	jsnc.Set("Closed", func(cb ...*js.Object) {
+	jsnc.Set("closed", nc.Closed)
+	jsnc.Set("ping", func(timeout float64, cb ...*js.Object) {
 		go func() {
-			r := nc.Closed()
-			ret(r, nil, cb)
-		}()
-	})
-
-	jsnc.Set("Ping", func(timeout int, cb ...*js.Object) {
-		go func() {
-			e := nc.Ping(time.Duration(timeout) * time.Second)
+			e := nc.Ping(time.Duration(timeout * float64(time.Second)))
 			ret(nil, e, cb)
 		}()
 	})
+	return jsnc
 }
--- nxjs/main.go
+++ nxjs/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/jaracil/nxcli"
 	"github.com/jaracil/nxcli/nxcore"
@@ -13,33 +11,29 @@ func main() {
 	// Node.js
 	if require := js.Global.Get("require"); require != js.Undefined {
 		if ws := require.Invoke("ws"); ws != js.Undefined {
-			fmt.Println("Running on Node.js with websocket module: 'ws'")
+			println("Running on Node.js with websocket module: 'ws'")
 
 			if removeListener := ws.Get("prototype").Get("removeEventListener"); removeListener == js.Undefined {
-				fmt.Println("Missing removeEventListener! Patching WebSocket library...")
+				println("Missing removeEventListener! Patching WebSocket library...")
 
 				ws.Get("prototype").Set("removeEventListener", js.MakeFunc(func(this *js.Object, args []*js.Object) interface{} {
 					this.Call("removeListener", args[0], args[1])
 					return nil
 				}))
 
-				fmt.Println("OK")
+				println("OK")
 			}
 
 			js.Global.Set("WebSocket", ws)
 		} else {
-			fmt.Println("Running on Node.js but websocket module missing")
+			println("Running on Node.js but websocket module missing")
 		}
 	}
 
 	dial := func(a string, cb func(interface{}, error)) {
 		go func() {
 			nc, e := nexus.Dial(a, nil)
-
-			jsnc := js.MakeWrapper(nc)
-			PatchNexusAsync(jsnc, nc)
-
-			go cb(jsnc, e)
+			go cb(WrapNexusConn(nc), e)
 		}()
 	}
 
@@ -47,15 +41,15 @@ func main() {
 
 	if js.Module == js.Undefined {
 		// Browser
-		js.Global.Set("Nexus", make(map[interface{}]interface{}))
-		nexus = js.Global.Get("Nexus")
+		js.Global.Set("nexus", make(map[interface{}]interface{}))
+		nexus = js.Global.Get("nexus")
 	} else {
 		// Node.js
 		nexus = js.Module.Get("exports")
 	}
 
-	nexus.Set("Dial", dial)
-	nexus.Set("Errors", nxcore.ErrStr)
+	nexus.Set("dial", dial)
+	nexus.Set("errors", nxcore.ErrStr)
 
-	fmt.Println("Nexus Client loaded")
+	println("Nexus Client loaded")
 }
