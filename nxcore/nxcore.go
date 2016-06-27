dd53496cb9be7eac29328de890cb1bb3f685b548
rogerzr <roger@nayarsystems.com>
2016-06-27 11:37:40 +0000

    Add Lock and Unlock methods to NexusConn

--- nxcore/nxcore.go
+++ nxcore/nxcore.go
@@ -538,6 +538,32 @@ func (nc *NexusConn) ChanPublish(channel string, msg interface{}) (interface{}, 
 	return nc.Exec("chan.pub", par)
 }
 
+// Lock tries to get a lock.
+// Returns lock success/failure or error.
+func (nc *NexusConn) Lock(lock string) (bool, error) {
+	par := ei.M{
+		"lock": lock,
+	}
+	res, err := nc.Exec("sync.lock", par)
+	if err != nil {
+		return false, err
+	}
+	return ei.N(res).M("ok").BoolZ(), nil
+}
+
+// Unlock tries to free a lock.
+// Returns unlock success/failure or error.
+func (nc *NexusConn) Unlock(lock string) (bool, error) {
+	par := ei.M{
+		"lock": lock,
+	}
+	res, err := nc.Exec("sync.unlock", par)
+	if err != nil {
+		return false, err
+	}
+	return ei.N(res).M("ok").BoolZ(), nil
+}
+
 // Close closes pipe.
 // Returns the response object from Nexus or error.
 func (p *Pipe) Close() (interface{}, error) {
